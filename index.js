82ccecb58455073d3955d3d46917b0cf8f7cfb52
Victor Kimathi <vkimathi97@gmail.com>
2025-03-18 14:05:32 +0300

    arithmentic-lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() *10) + 1
+
+let num3 = 29
+let num4 = 5
+let mod = 29 % 5
+
+let max = Math.max(16, 17, 18, 19, 20)
+console.log(max)
